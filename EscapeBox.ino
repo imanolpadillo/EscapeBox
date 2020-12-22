5f5e6e3a9f6ca26abea713c295d7010086c7d15c
imanolpadillo <67315499+imanolpadillo@users.noreply.github.com>
2020-12-22 14:58:02 +0100

    Create EscapeBox.ino

---
+++ EscapeBox.ino
@@ -0,0 +1,423 @@
+// ********************************************************************************************* //
+// ********************************************************************************************* //
+// ***************************************** ESCAPEBOX ***************************************** //
+// ********************************************************************************************* //
+// ********************************************************************************************* //
+// AUTOR:   Imanol Padillo
+// DATE:    21/12/2020
+// VERSION: 1.0
+
+// Board: Arduino Mega
+// Processor: Atmega 2560 (Mega 2560)
+// Port: /dev/cu.wchusbserial1420
+
+// Arduino project for developing an escape box, including the following steps:
+// Game_step_0:
+//   Place in order a set of wires with the clue of some leds
+// Game_step_1:
+//
+
+ 
+
+// ********************************************************************************************* //
+// INCLUDES 
+// ********************************************************************************************* //
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+// ********************************************************************************************* //
+// GPIO
+// ********************************************************************************************* //
+// GAME_STEP_LEDS
+#define GPIO_LED_0                        49
+#define GPIO_LED_1                        50
+#define GPIO_LED_2                        51
+#define GPIO_LED_3                        52
+#define GPIO_LED_4                        53
+// CLUES
+#define GPIO_CLUE                         2
+#define GPIO_BUZZER                       35
+// GAME STEP 0
+#define GPIO_PULSE                        3
+#define GPIO_LED_R                        22
+#define GPIO_LED_G                        23
+#define GPIO_LED_B                        24
+#define GPIO_LED_W                        25
+#define GPIO_WIRE_INPUT                   26
+// GAME STEP 1
+#define GPIO_KP_R1                        27
+#define GPIO_KP_R2                        28
+#define GPIO_KP_R3                        29
+#define GPIO_KP_R4                        30
+#define GPIO_KP_C1                        31
+#define GPIO_KP_C2                        32
+#define GPIO_KP_C3                        33
+#define GPIO_KP_C4                        34
+
+// ********************************************************************************************* //
+// CONSTANTS
+// ********************************************************************************************* //
+#define MAX_STEPS                         5
+#define MAX_CLUES                         3
+//game_step 1
+#define PASSWORD                          "1234"
+// lcd
+#define LCD_ADDRESS                       0x27
+#define LCD_SPEED                         300
+#define LCD_BUFFER                        40
+#define LCD_COLS                          16
+#define LCD_ROWS                          2
+#define TIMEOUT_SHORT                     2000
+#define TIMEOUT_LARGE                     3500
+// buzzer
+#define BUZZER_KEY                        0
+#define BUZZER_ERROR                      1
+#define BUZZER_SUCCESS                    2
+// Messages                                1234567890123456789012345678901234567890
+#define MSG_INIT_1                        "   ESCAPE BOX   "
+#define MSG_INIT_2                        "  Mai y Mario   "
+#define MSG_GAMESTEP0_1                   "PRUEBA 1        "
+#define MSG_GAMESTEP0_2                   "Conectad cables uuuuuu"
+#define MSG_GAMESTEP1_1                   "PRUEBA 2        "
+#define MSG_GAMESTEP1_2                   "Introduce 4 digitos + #"
+const String MSG_GAMESTEP[MAX_STEPS][LCD_ROWS] = {
+  {"Prueba 1", "Prueba1 - Descripcion"},
+  {"Prueba 2", "Prueba2 - Descripcion"},
+  {"Prueba 3", "Prueba3 - Descripcion"},
+  {"Prueba 4", "Prueba4 - Descripcion"},
+  {"Prueba 5", "Prueba5 - Descripcion"}
+};
+const String MSG_CLUE_1[MAX_STEPS][MAX_CLUES] = {
+  {"Prueba1 - Pista1", "Prueba1 - Pista2", "Prueba1 - Pista3"},
+  {"Prueba2 - Pista1", "Prueba2 - Pista2", "Prueba2 - Pista3"},
+  {"Prueba3 - Pista1", "Prueba3 - Pista2", "Prueba3 - Pista3"},
+  {"Prueba4 - Pista1", "Prueba4 - Pista2", "Prueba4 - Pista3"},
+  {"Prueba5 - Pista1", "Prueba5 - Pista2", "Prueba5 - Pista3"}
+};
+const String MSG_CLUE_2[MAX_STEPS][MAX_CLUES] = {
+  {"Prueba1 - Pista1", "Prueba1 - Pista2", "Prueba1 - Pista3"},
+  {"Prueba2 - Pista1", "Prueba2 - Pista2", "Prueba2 - Pista3"},
+  {"Prueba3 - Pista1", "Prueba3 - Pista2", "Prueba3 - Pista3"},
+  {"Prueba4 - Pista1", "Prueba4 - Pista2", "Prueba4 - Pista3"},
+  {"Prueba5 - Pista1", "Prueba5 - Pista2", "Prueba5 - Pista3"}
+};
+// ********************************************************************************************* //
+// VARIABLES
+// ********************************************************************************************* //
+// lcd
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+String lcd_msg_1;
+String lcd_msg_2;
+// keypad
+const char hexaKeys[4][4] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[4] = {GPIO_KP_R1, GPIO_KP_R2, GPIO_KP_R3, GPIO_KP_R4}; 
+byte colPins[4] = {GPIO_KP_C1, GPIO_KP_C2, GPIO_KP_C3, GPIO_KP_C4}; 
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, 4, 4); 
+// general
+int game_step = 0;
+String last_clue;
+bool flag_playing_clue = false;
+//game_step 0
+bool flag_playing_leds = false;
+//game_step 1
+String password_val;
+
+// ********************************************************************************************* //
+// SETUP
+// ********************************************************************************************* //
+void setup() {
+  // Setup digital pins
+  pinMode(GPIO_LED_0, OUTPUT); 
+  pinMode(GPIO_LED_1, OUTPUT); 
+  pinMode(GPIO_LED_2, OUTPUT); 
+  pinMode(GPIO_LED_3, OUTPUT); 
+  pinMode(GPIO_LED_4, OUTPUT); 
+  pinMode(GPIO_CLUE, INPUT_PULLUP);  
+  attachInterrupt(digitalPinToInterrupt(GPIO_CLUE), activate_flag_playing_clue, FALLING);
+  pinMode(GPIO_BUZZER, OUTPUT); 
+  pinMode(GPIO_LED_R, OUTPUT);  
+  pinMode(GPIO_LED_G, OUTPUT);  
+  pinMode(GPIO_LED_B, OUTPUT);  
+  pinMode(GPIO_LED_W, OUTPUT);  
+  pinMode(GPIO_PULSE, INPUT_PULLUP);  
+  attachInterrupt(digitalPinToInterrupt(GPIO_PULSE), activate_flag_playing_leds, FALLING);
+  pinMode(GPIO_WIRE_INPUT, INPUT);
+  pinMode(GPIO_KP_R1, INPUT);
+  pinMode(GPIO_KP_R2, INPUT);
+  pinMode(GPIO_KP_R3, INPUT);
+  pinMode(GPIO_KP_R4, INPUT);
+  pinMode(GPIO_KP_C1, INPUT);
+  pinMode(GPIO_KP_C2, INPUT);
+  pinMode(GPIO_KP_C3, INPUT);
+  pinMode(GPIO_KP_C4, INPUT);
+
+  // Setup serial port
+  Serial.begin(9600);  
+
+  // Init lcd
+  lcd.begin();                      
+  lcd.backlight();
+  lcd_print(MSG_INIT_1,MSG_INIT_2,TIMEOUT_LARGE);
+
+  // step leds
+  play_game_step_leds(-1);
+}
+
+// ********************************************************************************************* //
+// MAIN
+// ********************************************************************************************* //
+void loop() {
+  // Clue
+  if (flag_playing_clue == true){
+      play_clue(); 
+  }
+  
+  // GAME_STEP 0
+  if (game_step == 0)
+  {
+    lcd_print(MSG_GAMESTEP[game_step][0],MSG_GAMESTEP[game_step][1],0);
+    if (digitalRead(GPIO_WIRE_INPUT) == HIGH){
+      play_game_step_leds(game_step);
+      game_step = 1;
+    }
+    else if (flag_playing_leds == true){
+      play_leds(); 
+    }
+  }
+  // GAME_STEP_1
+  else if (game_step == 1)
+  {
+    lcd_print(MSG_GAMESTEP[game_step][0],MSG_GAMESTEP[game_step][1],0);
+    char customKey = customKeypad.getKey();
+    if (isAlphaNumeric(customKey) or isDigit(customKey)
+      or customKey == '*' or customKey == '#'){
+      play_buzzer(BUZZER_KEY);
+      if (check_password(customKey) == true){
+        play_game_step_leds(game_step);
+        game_step = 2;
+      }
+    }
+  }
+  
+}
+
+
+// ********************************************************************************************* //
+// ********************************************************************************************* //
+// FUNCTIONS
+// ********************************************************************************************* //
+// ********************************************************************************************* //
+
+// ********************************************************************************************* //
+// GENERAL
+// ********************************************************************************************* //
+void play_game_step_leds (int level)
+{
+  if (level == -1){
+    for (int i=0;i<MAX_STEPS;i++){
+      digitalWrite(GPIO_LED_0 + i,LOW);
+    }
+  }else{
+    for (int i=0;i<=level;i++){
+      digitalWrite(GPIO_LED_0 + i,HIGH);
+    }
+    play_buzzer(BUZZER_SUCCESS);
+  }
+}
+// Play buzzer tone
+void play_buzzer (int option)
+{
+  if (option == BUZZER_KEY){
+    tone(GPIO_BUZZER, 1000); 
+    delay(300);        
+    noTone(GPIO_BUZZER);     
+    delay(300);
+  }
+  else if (option == BUZZER_SUCCESS){
+    tone(GPIO_BUZZER, 3000); 
+    delay(800);        
+    noTone(GPIO_BUZZER);     
+    delay(300);
+  }
+  else if (option == BUZZER_ERROR){
+    tone(GPIO_BUZZER, 200); 
+    delay(300);        
+    noTone(GPIO_BUZZER);     
+    delay(300);
+  }
+   
+}
+
+// Print data in lcd display
+void lcd_print(String line1, String line2, int timeout)
+{
+  if (lcd_msg_1 != line1 and lcd_msg_2 != line2) {
+    lcd_msg_1 = line1;
+    lcd_msg_2 = line2;
+    lcd.home();
+    lcd.setCursor(0, 0);
+    lcd.print(line1);
+    lcd.setCursor(0, 1);
+    lcd.print(line2);
+    if (timeout != 0){
+      delay(timeout);
+      lcd.clear();
+    }
+  }
+  else {
+    lcd_scroll();
+  }
+  
+}
+
+// Scroll left lcd if text is longer than 16 characters
+void lcd_scroll()
+{
+  if (lcd_msg_1.length()>16 or lcd_msg_2.length()>16){
+    lcd.scrollDisplayLeft();
+    delay(LCD_SPEED);
+  }
+}
+
+// Get clue when interrupt is pressed
+void activate_flag_playing_clue()
+{
+  if (flag_playing_clue==false){
+    flag_playing_clue = true;
+  }
+}
+
+void play_clue ()
+{  
+  String clue_1;
+  String clue_2;
+  int index_last_clue = -1;
+  int index_next_clue;
+  
+  for (int i=0; i<MAX_CLUES; i++){
+    if (last_clue == MSG_CLUE_1[game_step][i]){
+      index_last_clue = i;
+      break;
+    }
+  }
+
+  if (index_last_clue == -1){
+    index_next_clue = 0;
+  }
+  else if (index_last_clue == MAX_CLUES-1){
+    index_next_clue = 0;
+  }
+  else{
+    index_next_clue = index_last_clue + 1;
+  }
+
+  
+  clue_1 = MSG_CLUE_1[game_step][index_next_clue];
+  clue_2 = MSG_CLUE_2[game_step][index_next_clue];
+  last_clue = clue_1;
+  
+  if (clue_2.length()>LCD_COLS){
+    for (int i=0; i<=LCD_BUFFER; i++){
+      lcd_print(clue_1, clue_2, 0);
+    }
+  }
+  else{
+    lcd_print(clue_1, clue_2, TIMEOUT_LARGE);
+  }
+  flag_playing_clue = false;
+}
+
+// ********************************************************************************************* //
+// GAME STEP 0
+// ********************************************************************************************* //
+
+// Interrupt function when pressing pulse
+void activate_flag_playing_leds()
+{
+  if (flag_playing_leds==false){
+    flag_playing_leds = true;
+  }
+}
+
+// Play leds with random timeout
+void play_leds ()
+{
+    long timeout;
+    timeout=random(50,800);
+    // 1/10
+    digitalWrite(GPIO_LED_R, HIGH);  // 2
+    digitalWrite(GPIO_LED_G, HIGH);  // 3
+    digitalWrite(GPIO_LED_B, HIGH);  // 1
+    digitalWrite(GPIO_LED_W, HIGH);  // 5
+    delay(timeout);
+    // 2/10
+    digitalWrite(GPIO_LED_W, LOW);
+    delay(timeout);
+    // 3/10
+    digitalWrite(GPIO_LED_G, LOW);
+    digitalWrite(GPIO_LED_R, LOW); 
+    digitalWrite(GPIO_LED_W, HIGH);
+    delay(timeout);
+    // 4/10
+    digitalWrite(GPIO_LED_G, HIGH);
+    digitalWrite(GPIO_LED_W, LOW);
+    delay(timeout);
+    // 5/10
+    digitalWrite(GPIO_LED_W, HIGH);
+    delay(timeout);
+    // 6/10
+    digitalWrite(GPIO_LED_R, HIGH); 
+    digitalWrite(GPIO_LED_G, LOW);
+    digitalWrite(GPIO_LED_B, LOW); 
+    digitalWrite(GPIO_LED_W, LOW);
+    delay(timeout);
+    // 7/10
+    digitalWrite(GPIO_LED_W, HIGH);
+    digitalWrite(GPIO_LED_G, HIGH);
+    delay(timeout);
+    // 8/10
+    digitalWrite(GPIO_LED_R, LOW); 
+    digitalWrite(GPIO_LED_W, LOW);
+    delay(timeout);
+    // 9/10
+    digitalWrite(GPIO_LED_G, HIGH);
+    digitalWrite(GPIO_LED_W, HIGH);
+    delay(timeout);
+    // 10/10
+    digitalWrite(GPIO_LED_G, LOW);
+    digitalWrite(GPIO_LED_W, LOW);
+    delay(timeout);
+  
+    digitalWrite(GPIO_LED_R, LOW);  
+    digitalWrite(GPIO_LED_G, LOW);  
+    digitalWrite(GPIO_LED_B, LOW);  
+    digitalWrite(GPIO_LED_W, LOW); 
+    flag_playing_leds = false; 
+}
+
+// ********************************************************************************************* //
+// GAME STEP 1
+// ********************************************************************************************* //
+
+// Interrupt function when pressing pulse
+bool check_password(char key)
+{
+  if (key=='#'){
+    if (password_val == PASSWORD){
+      return true;
+    }
+    else{
+      play_buzzer(BUZZER_ERROR);
+      password_val = "";
+    }
+  }
+  else{
+    password_val = password_val + key;
+  }
+  return false;
+}
